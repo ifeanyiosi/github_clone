e9028fb934e995f2af150e13bedd6d0c9e189bd9
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-05-04 14:52:01 +0100

    ......

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    
+    <>
+        <div className='mobile__nav-items' id='mobile__nav'>
+            <div className='mobile__header-search'>
+
+            </div>
+        </div>
+    </>
+       
+    
+  )
+}
+
+export default Header
