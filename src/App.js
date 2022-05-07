f2719ec60f51f7628fff3f59110d28612a44f854
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-05-07 01:52:28 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import HeaderDropdown from './components/HeaderDropdown';
-import Header from './components/HeaderMenu';
+import Header from './components/Header'
 import HeaderMenu from './components/HeaderMenu';
 import HeaderMobile from './components/HeaderMobile';
 
 function App() {
   return (
     <div className="App">
-      <HeaderMobile/>
+      <Header/>
     </div>
   );
 }
